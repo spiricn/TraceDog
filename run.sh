d623bdd28506d7e65d78e50dd4d858e5c05162bd
Nikola Spiric <nikola.spiric@rt-rk.com>
2015-01-16 17:41:14 +0100

    removed generated files & binaries

--- run.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-main(){
-	makec clean
-	makec all
-	LD_LIBRARY_PATH=`pwd` ./example
-}
-
-main "$@"
--- test.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-make clean && make all && sudo make install && make example && ./example
-
