0fc555785955ed3ebfdeba4988d67635c73ad3c3
Nikola Spiric <a href=mailto:nikola.spiric.ns@gmail.comnikola.spiric.ns@gmail.com/a>
2015-01-18 14:09:21 +0100

    examples project added

---
+++ source/examples/basic.c
@@ -0,0 +1,46 @@
+﻿#include <td/td.h>
+
+// First we need to define our module tag (if we're using TD wrapper macros)
+#undef TD_TRACE_TAG
+#define TD_TRACE_TAG "MyModule"
+
+int main(){
+	// Note that the console output is created by default
+
+	// Create a formatted HTML file output
+	TdOutputHandle html = td_createFileOutput("log.html", eTD_FMT_HTML);
+
+	// Create a plain text output
+	TdOutputHandle plain = td_createFileOutput("log.txt", eTD_FMT_PLAIN_TEXT);
+
+	// Basic log macros
+	LOGV("Verbose LOG");
+
+	LOGD("Debug LOG");
+
+	LOGI("Info LOG");
+
+	LOGW("Warning LOG");
+
+	LOGE("Error LOG");
+
+	// Tracing macro
+	TRACEV("Verbose TRACE");
+
+	TRACED("Debug TRACE");
+
+	TRACEI("Info TRACE");
+
+	TRACEW("Warning TRACE");
+
+	TRACEE("Error TRACE");
+
+	// Destroy previously created outputs
+	td_destroyOutput(html);
+
+	td_destroyOutput(plain);
+
+	system("pause");
+
+	return 0;
+}
